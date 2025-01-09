e72017621b338738764fccec977fd275d607f114
valio7 <v.karov25@gmail.com>
2025-01-09 18:29:59 +0200

    Add simple endpoint

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
+import express from "express"
 
+const app = express()
 
+app.get('http://localhost:3000/config', async (_req, res) => {
+    res.status(200).json("Hello, world!");
+});
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
