b0a5ca8eff3d9ab6363f03a9f74b497185e40081
valio7 <v.karov25@gmail.com>
2025-01-09 23:26:03 +0200

    Deploy on push to master

--- src/index.js
+++ src/index.js
@@ -11,5 +11,5 @@ dotenv.config()
 const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+    console.log(`Server is running`)
 })
